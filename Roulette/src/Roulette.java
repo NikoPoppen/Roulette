5fe7159100abb82fed40cca421a062d9d2be180a
Niko Poppen <nikopoppen@hotmail.de>
2017-02-24 22:04:26 +0100

    Alles auf Anfang

--- Roulette/src/Roulette.java
+++ Roulette/src/Roulette.java
@@ -4,7 +4,9 @@ public class Roulette {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int Blyat=0;
+		int Blyat=3;
+		for(int i=0;i<Blyat;i++)
+			System.out.println(Blyat);
 	}
 
 }
