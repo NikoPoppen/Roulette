f129400e0853674c5982c5585a9a716a794fc4a9
PatrickDuda <duda0patrick@gmail.com>
2017-02-25 19:30:15 +0100

    Test

--- Roulette/src/Roulette.java
+++ Roulette/src/Roulette.java
@@ -9,11 +9,11 @@ public class Roulette {
 
 		int summe = Zahl + 5;
 
-		
+
 		for(int i=0;i<Zahl;i++)
 			System.out.println(Zahl);
 		System.out.println("Endlich funzt das hier!");
-
+		int Penis = 15;
 
 	}
 
