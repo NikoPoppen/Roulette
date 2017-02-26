866c50c2ec84635c043799e967dbf3e69299fba2
Niko Poppen <nikopoppen@hotmail.de>
2017-02-26 19:54:39 +0100

    Ka

--- Roulette/src/Roulette.java
+++ Roulette/src/Roulette.java
@@ -13,17 +13,17 @@ public class Roulette {
 		for(int i=0;i<Zahl;i++)
 			System.out.println(Zahl);
 		System.out.println("Endlich funzt das hier!");
-<<<<<<< HEAD
+
 		int TEst3 =0;
 
 
-=======
+
 		int Get rkt = 15;
 		int grad =0;
 		int boi = 10;
 		int lmao = 0;
 
->>>>>>> refs/remotes/origin/MainQuellcode
+
 	}
 
 }
