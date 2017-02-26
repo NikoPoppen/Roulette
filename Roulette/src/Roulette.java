d28bf59ee1c573e4ca3999821cf84da6b3a987b6
Patrick Duda <duda0patrick@gmail.com>
2017-02-26 19:25:56 +0100

    Update Roulette.java

--- Roulette/src/Roulette.java
+++ Roulette/src/Roulette.java
@@ -13,8 +13,8 @@ public class Roulette {
 		for(int i=0;i<Zahl;i++)
 			System.out.println(Zahl);
 		System.out.println("Endlich funzt das hier!");
-		int Penis = 15;
-		MAdbad;
+		int Get rkt = 15;
+		
 
 	}
 
